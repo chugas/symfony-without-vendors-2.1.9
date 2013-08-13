6e7297f6938b255a52caf399243a465f777f43e9
Gaston Caldeiro <chugas488@gmail.com>
2013-08-13 16:39:18 -0300

    Multiples Generic Updates

--- src/Application/Success/UsuarioBundle/Entity/Repository/UserRepository.php
+++ src/Application/Success/UsuarioBundle/Entity/Repository/UserRepository.php
@@ -9,20 +9,5 @@ use Doctrine\ORM\EntityRepository;
  */
 class UserRepository extends EntityRepository
 {
-  public function getActiveColumnistas($limit){
-    $em = $this->getEntityManager();
 
-    $consulta = $em->createQuery("
-        SELECT u, n 
-        FROM UsuarioBundle:User u JOIN u.news n 
-        WHERE n.status = :status 
-        and n.published = 1 
-        and n.date_published <= CURRENT_TIMESTAMP() 
-        ORDER BY n.date_published DESC");
-
-    //and n.date_published > DATE_SUB(CURRENT_TIMESTAMP(), 1, 'MONTH')
-    $consulta->setParameter('status', 'aceptada');
-    $consulta->setMaxResults($limit);
-    return $consulta->getResult(); 
-  }
 }
